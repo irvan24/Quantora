bc85f6abfcb5249bbc146ae709d7432d6def87f3
irvan24 <irvan.assiakoley@epitech.eu>
2026-07-11 16:41:57 +0200

    [ADD] rganize backend package structure

--- backend/src/main/java/com/quantora/backend/QuaantoraApplication.java
+++ backend/src/main/java/com/quantora/backend/QuaantoraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class QuaantoraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(QuaantoraApplication.class, args);
 	}
 
 }
