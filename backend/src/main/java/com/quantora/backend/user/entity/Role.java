d60d308c5cce30487414fd941e48b1439cc383e2
irvan24 <irvan.assiakoley@epitech.eu>
2026-07-12 18:51:02 +0200

    [ADD] User entity an role enum

---
+++ backend/src/main/java/com/quantora/backend/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.quantora.backend.user.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/quantora/backend/user/entity/User.java
@@ -0,0 +1,49 @@
+package com.quantora.backend.user.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, unique = true, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
